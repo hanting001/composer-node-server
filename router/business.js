9d8012bb294da0ed8b7f6e8bd6a633b7dda071eb
hanting001 <hanting001@hotmail.com>
2018-11-23 16:58:29 +0800

    add join method

--- router/business.js
+++ router/business.js
@@ -2,6 +2,8 @@
 const errors = require('restify-errors');
 const bs = require('../lib/businessNetwork');
 const auth = require('../lib/auth');
+const config = require('config');
+const request = require('request-promise');
 
 module.exports = (server) => {
     // server.get('/balanceOf/:account', auth.jwt, auth.manager, async (req, res, next) => {
@@ -33,15 +35,27 @@ module.exports = (server) => {
             next(new errors.InternalServerError(err));
         }
     });
-    server.get('/product/flightDelay/join/:NO/:Date', async (req, res, next) => {
+    server.get('/product/join/:pid/:NO/:date',  async (req, res, next) => {
         try {
-            const serializer = bs.getSerializer();
-            const resource = serializer.fromJSON({
-                '$class': 'org.huibao.product.flightDelay.transaction.JoinTx',
-                'user': 'uid',
-                'flightInfo': 'fid'
-            });
-            const contract = await bs.submitTransaction(resource);
+            // check flight
+            const feichangzhun = config.get('feichangzhun.api');
+            const url = feichangzhun + `/flight/check/${req.params.NO}/${req.params.date}`;
+            const options = {
+                url: url,
+                json: true
+            }
+            const result = await request(options);
+            
+            const flightInfo = result.output;
+            // check user
+            const userRegistry = await bs.getParticipantRegistry('org.huibao.participant.User');
+            const user = await userRegistry.get('hanting');
+            //no exeption
+            const tx = bs.newTransaction('org.huibao.product.flightDelay.transaction', 'JoinTx');
+            tx.pid = req.params.pid;
+            tx.user = bs.newRelationship('org.huibao.participant', 'User', user.uid);
+            tx.flightInfo = bs.newRelationship('org.huibao.product.flightDelay.asset', 'FlightInfo', flightInfo.fid);
+            const contract = await bs.submitTransaction(tx);
             res.send({
                 output: contract
             });
