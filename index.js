ce3e2873cce70ba365bddcc98c7360c743399825
hanting001 <hanting001@hotmail.com>
2018-11-23 13:14:20 +0800

    fix bugs

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const restify = require('restify');
 const init = require('./lib/init');
 const server = restify.createServer();
-
-
 const router = require('./router');
+
+//系统初始化
+global.env = process.env.NODE_ENV;
 init(server);
 //配置路由
 router(server);
