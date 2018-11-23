2f95278560d5a5a8ae3e0f3e803f36a2581e6c5c
hanting001 <hanting001@hotmail.com>
2018-11-23 14:04:55 +0800

    add regist user method

--- router/business.js
+++ router/business.js
@@ -6,28 +6,32 @@ const auth = require('../lib/auth');
 module.exports = (server) => {
     // server.get('/balanceOf/:account', auth.jwt, auth.manager, async (req, res, next) => {
     server.post('/network/user/register', auth.jwt, async (req, res, next) => {
-        const uid = req.user.name;
-        const input = req.body.input;
-        if (!input.idNO) {
-            throw '证件号码必须录入';
+        try {
+            const uid = req.user.name;
+            const input = req.body.input;
+            if (!input.idNO) {
+                throw '证件号码必须录入';
+            }
+            const userRegistry = await bs.getParticipantRegistry('org.huibao.participant.User');
+            const exists = await userRegistry.exists(uid);
+            if (exists) {
+                res.send({
+                    output: '已注册用户'
+                });
+            } else {
+                const newUser = bs.newResource('org.huibao.participant', 'User', uid);
+                newUser.idNO = input.idNO;
+                newUser.name = req.user.fullName;
+                await bs.addParticipant('org.huibao.participant.User', newUser);
+                res.send({
+                    output: `成功注册新用户: ${newUser.uid}`
+                });
+            }
+
+            next();
+        } catch (err) {
+            next(new errors.InternalServerError(err));
         }
-        const userRegistry = await bs.getRegistry('org.huibao.participant.User');
-        const exists = await userRegistry.exists(uid);
-        if (exists) {
-            res.send({
-                output: '已注册用户'
-            });
-        } else {
-            const newUser = bs.newResource('org.huibao.participant', 'User', uid);
-            newUser.idNO = input.idNO;
-            newUser.name = req.user.fullName;
-            await bs.addResource('org.huibao.participant.User', newUser);
-            res.send({
-                output: `成功注册新用户: ${newUser.uid}`
-            });
-        }
-        
-        next();
     });
     server.get('/product/flightDelay/join/:NO/:Date', async (req, res, next) => {
         try {
@@ -39,7 +43,7 @@ module.exports = (server) => {
                 'user': 'uid',
                 'flightInfo': 'fid'
             });
-            const contract = await networkConnetion.submitTransaction(resource); 
+            const contract = await networkConnetion.submitTransaction(resource);
             res.send({
                 output: contract
             });
